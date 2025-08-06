1b97c7f7caa2da801dc4854feec814a6a010d0ba
Leandro Fonseca <leandromkt@outlook.com>
2025-08-05 21:12:58 -0300

    chore: configure tailwind css and create theme

---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
+        gray: {
+          900: "#66686D",
+          300: "#E8EAED",
+        },
+        orange: {
+          700: "#FD6841",
+        },
+        transparent: "transparent",
+        current: "currentColor",
+      },
+      fontSize:{
+        sm: 12,
+        md: 16,
+        lg: 24,
+        xl: 36,
+        xxl: 72,
+      },
+      extend: {
+        fontFamily: {
+          sans: ["Inter", "Arial", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+}
