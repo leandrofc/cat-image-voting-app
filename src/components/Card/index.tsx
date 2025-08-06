da9d4c9cff81f43daec7dfbc2801fb8913b06a65
Leandro Fonseca <leandromkt@outlook.com>
2025-08-06 16:50:58 -0300

    feat: adjust Card component to handle with voting in progress

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
 import { Button } from "../Button";
 import type { CardProps } from "./index.types";
+import { CatContext } from "../../context/CatContext";
 
 export const Card = (({ cat, onVote, voted }: CardProps) => {
     const { id, url, score } = cat;
+
+    const { voting: { isLoading, value }  } = useContext(CatContext)
+    
+    const votingUp = value > 0;
 
     return (
         <div className="p-5 m-10 border border-gray-300 rounded-3xl text-center w-[303px]">
@@ -21,15 +27,19 @@ export const Card = (({ cat, onVote, voted }: CardProps) => {
                     variant="icon"
                     icon="ThumbsUp"
                     onClick={() => onVote(id, 1)}
-                    disabled={voted}
+                    disabled={voted || isLoading}
                     isSelected={voted}
+                    score={score}
+                    isLoading={isLoading && votingUp}
                 />
                 <Button
                     variant="icon"
                     icon="ThumbsDown"
                     onClick={() => onVote(id, -1)}
-                    disabled={voted}
+                    disabled={voted || isLoading}
                     isSelected={voted}
+                    score={score}
+                    isLoading={isLoading && !votingUp}
                 />
             </div>
         </div>
