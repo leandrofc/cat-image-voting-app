3b9e1e7dadfc9927bd3e3eac4992192b00c434a7
Leandro Fonseca <leandromkt@outlook.com>
2025-08-06 13:03:45 -0300

    Create CardSkeleton component

---
+++ src/components/CardSkeleton/index.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "../ui/skeleton";
+
+export const CardSkeleton = (() => {
+    return (
+        <div className="p-5 m-10 border border-gray-300 rounded-3xl text-center w-[303px]">
+
+            <Skeleton className="h-[313px] w-[265px] bg-gray-300" />
+
+            <div className="flex justify-center w-full p-2 border-b border-gray-300">
+                <Skeleton className="h-[18px] w-[90px] rounded-xl bg-gray-300" />
+            </div>
+
+            <div className="flex justify-between h-auto mt-1">
+                <Skeleton className="h-[35px] w-[124px] rounded-xl bg-gray-300" />
+                <Skeleton className="h-[35px] w-[124px] rounded-xl bg-gray-300" />
+            </div>
+        </div>
+    )
+});
